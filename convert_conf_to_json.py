5a6709ca877c30de5014faa8b2e68ec186c21f0c
LaoT <ftkey@qq.com>
2024-10-11 20:22:15 +0800

    Update convert_conf_to_json.py

--- convert_conf_to_json.py
+++ convert_conf_to_json.py
@@ -1,30 +1,39 @@
 import json
 
 def parse_conf(conf_file):
-    rules = []
+    rules = {
+        "domain": [],
+        "domain_suffix": [],
+        "ip_cidr": []
+    }
     with open(conf_file, 'r') as file:
         for line in file:
             line = line.strip()
             if line.startswith('DOMAIN-SUFFIX,'):
-                rule = {"domain_suffix": line.split(',')[1].strip()}
-                rules.append(rule)
+                rules['domain_suffix'].extend([s for s in line.split(',')[1:] if s])
             elif line.startswith('IP-CIDR,'):
-                rule = {"ip_cidr": line.split(',')[1].strip()}
-                rules.append(rule)
-            elif line.startswith('DOMAIN-KEYWORD,'):
-                rule = {"domain_keyword": line.split(',')[1].strip()}
-                rules.append(rule)
-            elif line.startswith('DOMAIN,'):
-                rule = {"domain": line.split(',')[1].strip()}
-                rules.append(rule)
-            elif line.startswith('DOMAIN-SUFFIX,'):
-                rule = {"domain_suffix": line.split(',')[1].strip()}
-                rules.append(rule)
+                rules['ip_cidr'].extend([s for s in line.split(',')[1:] if s])
     return rules
+
+def merge_rules(base_rules, new_rules):
+    for key in new_rules:
+        if key in base_rules:
+            base_rules[key].extend(new_rules[key])
+        else:
+            base_rules[key] = new_rules[key]
+
+def convert_conf_to_json(conf_file, json_file):
+    base_rules = json.load(open(json_file, 'r'))
+    new_rules = parse_conf(conf_file)
+    merge_rules(base_rules, new_rules)
+    output = {"version": 1, "rules": merge_rules}
+
+    with open(json_file, 'w') as f:
+        json.dump(base_rules, f, indent=4)
 
 def convert_conf_to_json(conf_file, json_file):
     rules = parse_conf(conf_file)
-    output = {"version": 1, "rules": rules}
+    output = {"version": 1, "rules": [rules]}
     with open(json_file, 'w') as f:
         json.dump(output, f, indent=4)
 
